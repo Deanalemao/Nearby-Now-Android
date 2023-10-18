e1f710ed32f9feecae6c397ca027fe3bffb86ca7
Dean Alemao <deanalemao21@gmail.com>
2023-10-18 21:24:14 +0530

    added bottom navigation

--- app/src/main/java/com/codersden/nearbynow/MainActivity.java
+++ app/src/main/java/com/codersden/nearbynow/MainActivity.java
@@ -4,11 +4,21 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import com.etebarian.meowbottomnavigation.MeowBottomNavigation;
+
 public class MainActivity extends AppCompatActivity {
+
+    private MeowBottomNavigation bottomNavigation;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        bottomNavigation=findViewById(R.id.bottomNavigation);
+        bottomNavigation.show(2,true);
+
+        bottomNavigation.add(new MeowBottomNavigation.Model(1,R.drawable.home_icon));
+        bottomNavigation.add(new MeowBottomNavigation.Model(2,R.drawable.add));
+        bottomNavigation.add(new MeowBottomNavigation.Model(3,R.drawable.profile_icon));
     }
 }
